1637fb84109abab1033c6bd369375dd7ea7552cf
unknown <ismagandarillas@gmail.com>
2015-10-04 19:21:42 +0200

    job add debug info

--- app/Console/Commands/SyncPrestashopProducts.php
+++ app/Console/Commands/SyncPrestashopProducts.php
@@ -57,7 +57,7 @@ class SyncPrestashopProducts extends Command
     public function getProducts($user){
         try{
 
-            $webService = new PrestaShopWebservice(PS_SHOP_PATH, PS_WS_AUTH_KEY, DEBUG);
+            $webService = new PrestaShopWebservice($user->prestashop_url, $user->prestashop_api, DEBUG);
             // Here we set the option array for the Webservice : we want customers resources
             $opt['resource'] = 'products';
             $opt['display'] = '[name,id,description]';
