698294cd794f7cfcde7a02aa47c2c705e854f6db
unknown <ismagandarillas@gmail.com>
2015-10-04 15:17:24 +0200

    job add debug info

--- app/Commands/InsertClientFromPipedrive.php
+++ app/Commands/InsertClientFromPipedrive.php
@@ -39,7 +39,7 @@ class InsertClientFromPipedrive extends Command implements SelfHandling, ShouldB
 
         $dealId = $this->request['current']['id'];
         $orderData = $this->getOrderData($dealId);
-        error_log($orderData['data'][0]);
+        //error_log($orderData['data'][0]);
         $tools->addOrder($client,$orderData);
         error_log("FIN");
 
