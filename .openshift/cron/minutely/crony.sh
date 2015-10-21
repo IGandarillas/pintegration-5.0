1229816fdab409af40ed17607c51b65d9d2ad6fc
igandarillas <ismagandarillas@gmail.com>
2015-10-21 03:30:56 +0200

    update

--- .openshift/cron/minutely/crony.sh
+++ .openshift/cron/minutely/crony.sh
@@ -1,3 +1,3 @@
  #!/bin/bash
-php /var/lib/openshift/561e79980c1e666eb40000dd/app-root/repo/artisan schedule:run 1>> /dev/null 2>&1
+#php /var/lib/openshift/561e79980c1e666eb40000dd/app-root/repo/artisan schedule:run 1>> /dev/null 2>&1
 
